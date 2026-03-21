b6109a63342c4b9ef7d2e6eea74f157570d981d3
workwithvaibhav7 <workwithvaibhav7@gmail.com>
2026-03-21 21:30:05 +0530

    Add files via upload

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -26,7 +26,7 @@ const textures = imageUrls.map((url) => textureLoader.load(url));
 
 const sphereGeometry = new THREE.SphereGeometry(1, 28, 28);
 
-const spheres = [...Array(30)].map(() => ({
+const spheres = [...Array(15)].map(() => ({
   scale: [0.7, 1, 0.8, 1, 1][Math.floor(Math.random() * 5)],
 }));
 
