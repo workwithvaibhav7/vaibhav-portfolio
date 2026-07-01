31b836c2200fc8d99e3ba5d769ea2ef430e718f5
workwithvaibhav7 <workwithvaibhav7@gmail.com>
2026-07-02 03:49:09 +0530

    Update TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -24,9 +24,9 @@ const imageUrls = [
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
-const sphereGeometry = new THREE.SphereGeometry(1, 28, 28);
+const sphereGeometry = new THREE.SphereGeometry(1, 16, 16);
 
-const spheres = [...Array(15)].map(() => ({
+const spheres = [...Array(10)].map(() => ({
   scale: [0.7, 1, 0.8, 1, 1][Math.floor(Math.random() * 5)],
 }));
 
